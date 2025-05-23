7505fae191136d22c906de801ceea172688438b5
Kaleb9301 <Kalebwondwossent@gmail.com>
2025-05-23 18:00:48 +0300

    Add: creating Models

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -78,6 +78,18 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+
+    'postgres': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'backend_final_project',
+        'USER': 'postgres',
+        'PASSWORD': 'goDjango@2015#',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        # 'OPTIONS' : {
+        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        # }
     }
 }
 
--- LittleLemon/Restaurant/urls.py
+++ LittleLemon/Restaurant/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = {
-    path('/sayhello', views.sayHello, name='sayHello'),
+urlpatterns = [
+    path('sayhello', views.sayHello, name='sayHello'),
     path('', views.index, name='index')
-}
+]
